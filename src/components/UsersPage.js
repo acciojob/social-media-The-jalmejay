000043e70c9fed512addca475b8e2cea24e6ed1b
The-jalmejay <jalmejaykumarshaw10@gmail.com>
2025-11-19 00:13:55 +0530

    user

--- src/components/UsersPage.js
+++ src/components/UsersPage.js
@@ -47,7 +47,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-export default function Users() {
+export default function UsersPage() {
   const users = useSelector((state) => state.users);
 
   return (
