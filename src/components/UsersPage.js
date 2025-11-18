4288d77c639dd95549e73132b83476dc9aae9c9e
The-jalmejay <jalmejaykumarshaw10@gmail.com>
2025-11-19 00:13:03 +0530

    userpage

--- src/components/UsersPage.js
+++ src/components/UsersPage.js
@@ -1,44 +1,65 @@
-// src/components/UsersPage.js
-import React, { useState } from "react";
+// // src/components/UsersPage.js
+// import React, { useState } from "react";
 
-function UsersPage({ users, posts }) {
-  const [selectedUserId, setSelectedUserId] = useState(null);
+// function UsersPage({ users, posts }) {
+//   const [selectedUserId, setSelectedUserId] = useState(null);
 
-  const visiblePosts = posts.filter((p) => p.authorId === selectedUserId);
+//   const visiblePosts = posts.filter((p) => p.authorId === selectedUserId);
+
+//   return (
+//     <div className="container">
+//       {/* <h2 className="sectionTitle">Users</h2> */}
+
+//       <div className="usersList">
+//         <ul>
+//           {users.map((u) => (
+//             <li key={u.id}>
+//               {/* Cypress uses :nth-child(3) > a etc. */}
+//               <a
+//                 href="#"
+//                 onClick={(e) => {
+//                   e.preventDefault();
+//                   setSelectedUserId(u.id);
+//                 }}
+//               >
+//                 {u.name}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+
+//       <section>
+//         {visiblePosts.map((p) => (
+//           <article key={p.id} className="post">
+//             <h3>{p.title}</h3>
+//             <p>{p.content}</p>
+//           </article>
+//         ))}
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default UsersPage;
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export default function Users() {
+  const users = useSelector((state) => state.users);
 
   return (
     <div className="container">
-      {/* <h2 className="sectionTitle">Users</h2> */}
-
-      <div className="usersList">
-        <ul>
-          {users.map((u) => (
-            <li key={u.id}>
-              {/* Cypress uses :nth-child(3) > a etc. */}
-              <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setSelectedUserId(u.id);
-                }}
-              >
-                {u.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <section>
-        {visiblePosts.map((p) => (
-          <article key={p.id} className="post">
-            <h3>{p.title}</h3>
-            <p>{p.content}</p>
-          </article>
+      <h2 className="sectionTitle">Users</h2>
+      <ul className="usersList">
+        {users.map((u) => (
+          <li key={u}>
+            <Link to={`/users/${u}`}>{u}</Link>
+          </li>
         ))}
-      </section>
+      </ul>
     </div>
   );
 }
-
-export default UsersPage;
